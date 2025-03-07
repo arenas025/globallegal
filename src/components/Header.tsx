b62a38d8bfc0cd502d8e563db4d30e47c41f1d2e
arenas025 <sergioarenas025@gmail.com>
2025-03-07 16:09:01 -0500

    finished the missing things

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./Header.css";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="header">
       <div className="hero-logo">
         <img width="200" height="40" src="/logo.svg" alt="global legal logo" />
-        <img width="20" height="20" src="/burger-menu.svg" alt="menu" />
+        <img
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          width="20"
+          height="20"
+          src="/burger-menu.svg"
+          alt="menu"
+        />
       </div>
       <ul>
         <li>
@@ -18,6 +26,25 @@ const Header = () => {
           <a href="#contact">Contáctanos</a>
         </li>
       </ul>
+      {isMenuOpen && (
+        <div className="menu-overlay">
+          <div className="menu-overlay-header">
+            <img src="/logo.svg" alt="global legal logo" />
+            <p onClick={() => setIsMenuOpen(false)}>x</p>
+          </div>
+          <ul>
+            <li>
+              <a href="#service-offering">Nuestros servicios</a>
+            </li>
+            <li>
+              <a href="#team">Conoce a nuestro equipo</a>
+            </li>
+            <li>
+              <a href="#contact">Contáctanos</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
